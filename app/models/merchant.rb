c0dd1f03d3f21be7b70fa116e5c75eb2aba93c5d
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-11-27 16:45:09 -0700

    Merchant.ranked_by_revenue wip

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,11 @@
 class Merchant < ApplicationRecord
+
+  def self.ranked_by_revenue(limit)
+    transaction_ids = Transaction.where(result: :success)
+      .pluck(:invoice_id)
+    invoice_ids = Invoice.find(transaction_ids)
+      .pluck(ids)
+
+  end
+
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
 
-  it '.rank_by_revenue(limit)' do
+  xit '.rank_by_revenue(limit)' do
     transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3)
-    item_1 = create(:item, unit_price: 200)
-    item_2 = create(:item, unit_price: 1000)
-    item_3 = create(:item, unit_price: 500)
-    transaction_1.invoice.items = [item_1]
-    transaction_2.invoice.items = [item_2]
-    transaction_3.invoice.items = [item_3]
+    invoice_item_1 = create(:invoice_item, unit_price: 5, quantity: 1)
+    invoice_item_2 = create(:invoice_item, unit_price: 9, quantity: 1)
+    invoice_item_3 = create(:invoice_item, unit_price: 2, quantity: 1)
+    transaction_1.invoice.invoice_items = [invoice_item_1]
+    transaction_2.invoice.invoice_items = [invoice_item_2]
+    transaction_3.invoice.invoice_items = [invoice_item_3]
 
-    expected = [item_2.merchant]
-    expect(Merchant.ranked_by_revenue(1)).to eq(expected)
 
-    expected = [item_2.merchant, item_3,merchant]
-    expect(Merchant.ranked_by_revenue(2)).to eq(expected)
+    expected = [invoice_item_2.item.merchant]
+    actual = Merchant.ranked_by_revenue(1)
+    expect(actual).to eq(expected)
+
+    expected = [invoice_item_2.item.merchant, invoice_item_3.item,merchant]
+    actual = Merchant.ranked_by_revenue(2)
+    expect(actual).to eq(expected)
   end
 
 end
