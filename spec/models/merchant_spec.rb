6054a6c52dc5a6cbe9d2ee601bf3d2c963dc3911
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-11-27 15:46:35 -0700

    Write Merchant.ranked_by_revenue(limit) test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,17 +3,13 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :model do
 
   it '.rank_by_revenue(limit)' do
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3)
     item_1 = create(:item, unit_price: 200)
     item_2 = create(:item, unit_price: 1000)
     item_3 = create(:item, unit_price: 500)
-    binding.pry
-    
-
-    invoice_1 = create(:invoice, merchant: item_1.merchant)
-    invoice_2 = create(:invoice, merchant: item_2.merchant)
-    invoice_3 = create(:invoice, merchant: item_3.merchant)
-
-    binding.pry
+    transaction_1.invoice.items = [item_1]
+    transaction_2.invoice.items = [item_2]
+    transaction_3.invoice.items = [item_3]
 
     expected = [item_2.merchant]
     expect(Merchant.ranked_by_revenue(1)).to eq(expected)
