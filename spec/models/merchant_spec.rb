3298d04bfdfe58e69266566ddf289f77e5bb16f1
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-11-28 17:53:50 -0700

    Write Merchant.rank_by_items_sold_count(limit)

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,39 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  describe 'class methods' do
+    before(:each) do
+      @ii_1 = create(:invoice_item, unit_price: 5, quantity: 1)
+      @ii_1.item.merchant = @ii_1.invoice.merchant
+      @ii_1.invoice.transactions.create(attributes_for(:transaction))
 
-  it '.rank_by_revenue(limit)' do
-    ii_1 = create(:invoice_item, unit_price: 5)
-    ii_1.item.merchant = ii_1.invoice.merchant
-    ii_1.invoice.transactions.create(attributes_for(:transaction))
+      @ii_2 = create(:invoice_item, unit_price: 9, quantity: 1)
+      @ii_2.item.merchant = @ii_2.invoice.merchant
+      @ii_2.invoice.transactions.create(attributes_for(:transaction))
 
-    ii_2 = create(:invoice_item, unit_price: 9)
-    ii_2.item.merchant = ii_2.invoice.merchant
-    ii_2.invoice.transactions.create(attributes_for(:transaction))
+      @ii_2 = create(:invoice_item, unit_price: 1, quantity: 3)
+      @ii_2.item.merchant = @ii_2.invoice.merchant
+      @ii_2.invoice.transactions.create(attributes_for(:transaction))
 
-    ii_2 = create(:invoice_item, unit_price: 1)
-    ii_2.item.merchant = ii_2.invoice.merchant
-    ii_2.invoice.transactions.create(attributes_for(:transaction))
+      @ii_3 = create(:invoice_item, unit_price: 4, quantity: 10)
+      @ii_3.item.merchant = @ii_3.invoice.merchant
+      @ii_3.invoice.transactions.create(attributes_for(:transaction))
 
-    ii_3 = create(:invoice_item, unit_price: 4, quantity: 10)
-    ii_3.item.merchant = ii_3.invoice.merchant
-    ii_3.invoice.transactions.create(attributes_for(:transaction))
+      @ii_4 = create(:invoice_item, unit_price: 5, quantity: 2)
+      @ii_4.item.merchant = @ii_4.invoice.merchant
+      @ii_4.invoice.transactions.create(attributes_for(:transaction))
+    end
 
-    ii_4 = create(:invoice_item, unit_price: 5, quantity: 2)
-    ii_4.item.merchant = ii_4.invoice.merchant
-    ii_4.invoice.transactions.create(attributes_for(:transaction))
+    it '.rank_by_revenue(limit)' do
 
-    expected_first = ii_3.item.merchant
-    expected_second = ii_4.item.merchant
-    expected_last = ii_1.item.merchant
+      expected_first = @ii_3.item.merchant
+      expected_second = @ii_4.item.merchant
+      expected_last = @ii_1.item.merchant
 
-    actual_first = Merchant.ranked_by_revenue(4).first
-    actual_second = Merchant.ranked_by_revenue(4).second
-    actual_last = Merchant.ranked_by_revenue(4).last
+      actual_first = Merchant.ranked_by_revenue(4).first
+      actual_second = Merchant.ranked_by_revenue(4).second
+      actual_last = Merchant.ranked_by_revenue(4).last
 
-    expect(actual_first).to eq(expected_first)
-    expect(actual_second).to eq(expected_second)
-    expect(actual_last).to eq(expected_last)
+      expect(actual_first).to eq(expected_first)
+      expect(actual_second).to eq(expected_second)
+      expect(actual_last).to eq(expected_last)
+    end
+
+    it '.rank_by_items_sold(limit)' do
+      expected_first = @ii_3.item.merchant
+      expected_second = @ii_2.item.merchant
+      expected_last = @ii_4.item.merchant
+
+      actual_first = Merchant.ranked_by_items_sold(3).first
+      actual_second = Merchant.ranked_by_items_sold(3).second
+      actual_last = Merchant.ranked_by_items_sold(3).last
+
+      expect(actual_first).to eq(expected_first)
+      expect(actual_second).to eq(expected_second)
+      expect(actual_last).to eq(expected_last)
+    end
+
+
   end
-
 end
