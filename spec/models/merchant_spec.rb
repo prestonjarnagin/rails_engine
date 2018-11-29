6c98798cb3fcacd832cea72c33c8e41f14994643
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-11-29 10:00:36 -0700

    Update merchant test and test data

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,34 +5,33 @@ RSpec.describe Merchant, type: :model do
     before(:each) do
       @ii_1 = create(:invoice_item, unit_price: 5, quantity: 1)
       @ii_1.item.merchant = @ii_1.invoice.merchant
-      @ii_1.update(created_at: "2012-01-01 14:40:00 UTC")
+      @ii_1.invoice.update(created_at: "2012-01-01 14:40:00 UTC")
       @ii_1.invoice.transactions.create(attributes_for(:transaction))
 
       @ii_2 = create(:invoice_item, unit_price: 9, quantity: 1)
       @ii_2.item.merchant = @ii_2.invoice.merchant
-      @ii_1.update(created_at: "2012-01-01 14:50:00 UTC")
+      @ii_2.invoice.update(created_at: "2012-01-01 14:50:00 UTC")
       @ii_2.invoice.transactions.create(attributes_for(:transaction))
 
-      @ii_2 = create(:invoice_item, unit_price: 1, quantity: 3)
-      @ii_2.item.merchant = @ii_2.invoice.merchant
-      @ii_1.update(created_at: "2012-01-02 14:54:09 UTC")
-      @ii_2.invoice.transactions.create(attributes_for(:transaction))
-
-      @ii_3 = create(:invoice_item, unit_price: 4, quantity: 10)
+      @ii_3 = create(:invoice_item, unit_price: 1, quantity: 3)
       @ii_3.item.merchant = @ii_3.invoice.merchant
-      @ii_1.update(created_at: "2012-01-02 14:54:09 UTC")
+      @ii_3.invoice.update(created_at: "2012-01-02 14:54:09 UTC")
       @ii_3.invoice.transactions.create(attributes_for(:transaction))
 
-      @ii_4 = create(:invoice_item, unit_price: 5, quantity: 2)
+      @ii_4 = create(:invoice_item, unit_price: 4, quantity: 10)
       @ii_4.item.merchant = @ii_4.invoice.merchant
-      @ii_1.update(created_at: "2012-01-03 14:54:09 UTC")
+      @ii_4.invoice.update(created_at: "2012-01-02 14:54:09 UTC")
       @ii_4.invoice.transactions.create(attributes_for(:transaction))
+
+      @ii_5 = create(:invoice_item, unit_price: 5, quantity: 2)
+      @ii_5.item.merchant = @ii_5.invoice.merchant
+      @ii_5.invoice.update(created_at: "2012-01-03 14:54:09 UTC")
+      @ii_5.invoice.transactions.create(attributes_for(:transaction))
     end
 
     it '.rank_by_revenue(limit)' do
-
-      expected_first = @ii_3.item.merchant
-      expected_second = @ii_4.item.merchant
+      expected_first = @ii_4.item.merchant
+      expected_second = @ii_5.item.merchant
       expected_last = @ii_1.item.merchant
 
       actual_first = Merchant.ranked_by_revenue(4).first
@@ -45,9 +44,9 @@ RSpec.describe Merchant, type: :model do
     end
 
     it '.rank_by_items_sold_count(limit)' do
-      expected_first = @ii_3.item.merchant
-      expected_second = @ii_2.item.merchant
-      expected_last = @ii_4.item.merchant
+      expected_first = @ii_4.item.merchant
+      expected_second = @ii_3.item.merchant
+      expected_last = @ii_5.item.merchant
 
       actual_first = Merchant.ranked_by_items_sold_count(3).first
       actual_second = Merchant.ranked_by_items_sold_count(3).second
@@ -58,7 +57,7 @@ RSpec.describe Merchant, type: :model do
       expect(expected_last).to eq(actual_last)
     end
 
-    it '.revenue_on_date(date)' do
+    xit '.revenue_on_date(date)' do
       expected = 14
       actual = Merchant.revenue_on_date("2012-01-01")
       expect(actual).to eq(expected)
