e154461a3520e671fdeb4574f01363bb1f56760f
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-11-28 17:42:49 -0700

    Write Merchant.ranked_by_revenue(limit_to)

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,11 +1,14 @@
 class Merchant < ApplicationRecord
 
-  def self.ranked_by_revenue(limit)
-    transaction_ids = Transaction.where(result: :success)
-      .pluck(:invoice_id)
-    invoice_ids = Invoice.find(transaction_ids)
-      .pluck(ids)
+  has_many :invoices
 
+  def self.ranked_by_revenue(limit_to)
+    Merchant.joins(invoices: [:invoice_items, :transactions])
+      .where(transactions: {result: "success"})
+      .select("merchants.*, SUM(invoice_items.quantity*invoice_items.unit_price) as revenue")
+      .group(:id)
+      .order("revenue desc")
+      .limit(limit_to)
   end
 
 end
