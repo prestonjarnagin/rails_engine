46844050419baef41cdc58e921b90ea8bcefa601
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-01 11:00:18 -0700

    Item.rank_by_quantity sold method and spec

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,19 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   def self.rank_by_revenue(limit_to)
-      Item.joins(:invoice_items, invoices: [:transactions])
-        .merge(Transaction.unscoped.successful)
-        .select('items.id, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue')
-        .group('items.id')
-        .order('revenue DESC')
-        .limit(limit_to)
+    joins(:invoice_items, invoices: [:transactions])
+      .merge(Transaction.unscoped.successful)
+      .select('items.id, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue')
+      .group('items.id')
+      .order('revenue DESC')
+      .limit(limit_to)
   end
 
+  def self.rank_by_quantity_sold(limit_to)
+    joins(:invoice_items, invoices: [:transactions])
+      .merge(Transaction.unscoped.successful)
+      .select('items.id, SUM(invoice_items.quantity) AS quantity')
+      .group(:id)
+      .order('quantity DESC')
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,17 +4,45 @@ RSpec.describe Item, type: :model do
 
   describe 'class methods' do
 
-    it '.rank_by_revenue' do
-      ii_1 = create(:invoice_item, quantity: 5, unit_price: 10)
-      ii_2 = create(:invoice_item, quantity: 3, unit_price: 3)
-      ii_3 = create(:invoice_item, quantity: 1, unit_price: 5)
-      ii_4 = create(:invoice_item, quantity: 2, unit_price: 1)
-      ii_5 = create(:invoice_item, quantity: 1, unit_price: 1)
-      ii_6 = create(:invoice_item, quantity: 10, unit_price: 10)
+    before(:each) do
+      @ii_1 = create(:invoice_item, quantity: 5, unit_price: 10)
+      @ii_1.invoice.transactions.create(attributes_for(:transaction))
 
-      expected = [ii_6.item, ii_1.item, ii_2.item]
+      @ii_2 = create(:invoice_item, quantity: 1, unit_price: 5)
+      @ii_2.invoice.transactions.create(attributes_for(:transaction))
+
+      @ii_3 = create(:invoice_item, quantity: 3, unit_price: 3)
+      @ii_3.invoice.transactions.create(attributes_for(:transaction))
+
+      @ii_4 = create(:invoice_item, quantity: 8, unit_price: 1)
+      @ii_4.invoice.transactions.create(attributes_for(:transaction))
+
+      @ii_5 = create(:invoice_item, quantity: 1, unit_price: 1)
+      @ii_5.invoice.transactions.create(attributes_for(:transaction))
+
+      @ii_6 = create(:invoice_item, quantity: 10, unit_price: 10)
+      @ii_6.invoice.transactions.create(attributes_for(:transaction))
+    end
+
+    it '.rank_by_revenue' do
+      expected_first = @ii_6.item
+      expected_second = @ii_1.item
+      expected_third = @ii_3.item
       actual = Item.rank_by_revenue(3)
-      expect(actual).not_to eq(expected)
+      expect(actual.first).to eq(expected_first)
+      expect(actual.second).to eq(expected_second)
+      expect(actual.third).to eq(expected_third)
+    end
+
+    it '.rank_by_quantity_sold' do
+      expected_first = @ii_6.item
+      expected_second = @ii_4.item
+      expected_third = @ii_1.item
+      actual = Item.rank_by_quantity_sold(3)
+
+      expect(actual.first).to eq(expected_first)
+      expect(actual.second).to eq(expected_second)
+      expect(actual.third).to eq(expected_third)
     end
   end
 
