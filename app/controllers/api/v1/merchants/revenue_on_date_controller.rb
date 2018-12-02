61fc6f7449b90fa6a4d871cfd3e178a17b2db9ec
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-02 16:36:54 -0700

    Remove commented out code

--- app/controllers/api/v1/merchants/revenue_on_date_controller.rb
+++ app/controllers/api/v1/merchants/revenue_on_date_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::Merchants::RevenueOnDateController < ApplicationController
 
   def show
-    # revenue_in_cents = Merchant.revenue_on_date(params[:date])
-    # revenue_in_dollars = "%.2f" % Rational("#{revenue_in_cents}".to_i,100)
-    # response = {'data' => {"attributes" => {"revenue" => revenue_in_dollars}}}
-    # render json: response
-
     revenue = Revenue.new(Merchant.revenue_on_date(params[:date]))
     render json: RevenueSerializer.new(revenue)
   end
